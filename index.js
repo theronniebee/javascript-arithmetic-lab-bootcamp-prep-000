3cd86f439aaf7af77681b8c9ad1a008b91b65376
Laronda Bell <larondak.bell@gmail.com>
2019-07-19 20:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function add(a, b){
   c = a + b;
   return c;
 }
+function subtract(a, b){
+  c = a - b;
+  return c;
+}
+function multiply(a, b){
+  c = a* b;
+  return c;
+}
+function divide(a, b){
+  c = a + b;
+  return c;
+}
