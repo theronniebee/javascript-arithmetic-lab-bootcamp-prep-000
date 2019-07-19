e495e1da1811a1a22234339fe70873afc311f581
Laronda Bell <larondak.bell@gmail.com>
2019-07-19 20:40:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-var c;
-function add(a, b){
-  c = a + b;
-  return c;
-}
-function subtract(a, b){
-  c = a - b;
-  return c;
-}
-function multiply(a, b){
-  c = a* b;
-  return c;
-}
-function divide(a, b){
-  c = a/b;
-  return c;
-  
-}
-function inc(a){
-  c = a+1;
-  return c;
-}
-
-function dec(a){ 
-  number = a-1;
+var number;
+function add(a, b){ //add a and b and return the result.
+  number = a + b;
   return number;
 }
-function makeInt(a){ 
+function subtract(a, b){ //subtract a and b and return the result.
+  number = a - b;
+  return number;
+}
+function multiply(a, b){ //multiply a and b and return the result.
+  number = a * b;
+  return number;
+}
+function divide(a, b){ //divide a and b and return the result.
+  number = a / b;
+  return number;
+}
+function inc(a){ //Increment a by 1 and return the result.
+  number = a + 1;
+  return number;
+}
+function dec(a){ //Decrement a by 1 and return the result.
+  number = a - 1;
+  return number;
+}
+function makeInt(a){ //Parse a as an integer and return that integer.
   number = parseInt(a, 10);
   return number;
 }
-function preserveDecimal(a){  
+function preserveDecimal(a){ //Parse a as a float (number with decimals) and return that number.
   number = parseFloat(a, 10);
   return number;
 }
