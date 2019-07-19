104673100fcb05947431a27fffcc1e6cfe792704
Laronda Bell <larondak.bell@gmail.com>
2019-07-19 20:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,12 @@ function dec(a){
   number = a-1;
   return number;
 }
-
+function makeInt(a){  integer.
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){  decimals) and return that number.
+  number = parseFloat(a, 10);
+  return number;
+}
 
